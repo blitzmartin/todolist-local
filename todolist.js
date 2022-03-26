1171897c581fa09ad4aa96af984ba553415c4a58
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-03-26 19:35:24 +0100

    cleaner code

--- todolist.js
+++ todolist.js
@@ -1,11 +1,16 @@
 //SELECTORS VARIABLE
+
 const todoInput = document.querySelector('.todo-input'); // the input field
 const todoButton = document.querySelector('.todo-button'); // the + button
 const todoList = document.querySelector('.todo-list');  // the list (ul) that's initially empty
 
 
+
 //EVENT LISTENERS
+
 todoButton.addEventListener('click', addTodo);  //adds event listener to + button
+
+
 
 //FUNCTIONS
 
@@ -14,43 +19,40 @@ function addTodo(event) {  //when button is clicked do this things
     //Prevent form from submitting (Chrome keeps refreshing by itself)
     event.preventDefault();
 
-    if (todoInput.value.length > 0) { //checks that input is not empty
-
-        //Create new ToDo div, it will contain new list item
-        const todoDiv = document.createElement('div'); //create a new div
-        todoDiv.classList.add('todo'); //style div
+    if (todoInput.value.length > 0) {       // the function only happens if input is not empty
 
         //Create label that will contain input checkbox and span-text
         const itemLabel = document.createElement('label');
-        itemLabel.setAttribute("for", "newItem");
+        itemLabel.setAttribute("for", "newItem");  //new attribute for=newItem inside label tag
 
         //Create input checkbox
         const checkbox = document.createElement('input');
-        checkbox.setAttribute("type", "checkbox");
-        checkbox.setAttribute("id", "newItem");  //give type attribute of checkbox to input
-        itemLabel.appendChild(checkbox);
+        checkbox.setAttribute("type", "checkbox");  //new attribute type=checkbox inside input tag
+        checkbox.setAttribute("id", "newItem");  //new attribute id=newItem inside input tag
+        itemLabel.appendChild(checkbox);  //append input checkbox to label
 
         //Create span to receive input text
         const spanText = document.createElement('span');
         spanText.innerHTML = todoInput.value;
-        itemLabel.appendChild(spanText);
+        itemLabel.appendChild(spanText); //append span with text to label (after checkbox)
 
         //Create new Li element
         const listItem = document.createElement('li');  //create a new list item
-        /* newTodo.innerHTML = todoInput.value;  */  //write in new li the input value
         listItem.classList.add('todo-item');
-        listItem.appendChild(itemLabel);
+        listItem.appendChild(itemLabel); //put label inside list item
 
-        todoDiv.appendChild(listItem); //put the new li inside new div
+        //Create new ToDo div, it will contain a new list item
+        const listDiv = document.createElement('div'); //create a new div
+        listDiv.classList.add('todo'); //style div
 
-        //Append new div to ToDo List
-        todoList.appendChild(todoDiv);
+        //Put the new li inside new div
+        listDiv.appendChild(listItem);
 
-        //Check what happens in console
-        console.log(todoDiv);
+        //Put the div inside the big unordered list container
+        todoList.appendChild(listDiv);
 
         //Clear input field
         todoInput.value = "";
     }
-    
+
 }
